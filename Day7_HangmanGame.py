dc143c92ea7f28167650f0610304fc590436ac22
Ivonne Hartono <65146516+NWatcher12@users.noreply.github.com>
2025-12-04 20:43:43 -0800

    Day 7 Hangman Game

---
+++ Day7_HangmanGame.py
@@ -0,0 +1,344 @@
+import random
+import sys
+
+hangman = [ """ 
+____
+|/   |
+|   
+|    
+|    
+|    
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|    
+|    
+|    
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|    |
+|    |    
+|    
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|   \|
+|    |
+|    
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|   \|/
+|    |
+|    
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|   \|/
+|    |
+|   / 
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|   \|/
+|    |
+|   / \
+|
+|_____
+""",
+"""
+ ____
+|/   |
+|   (_)
+|   /|\
+|    |
+|   | |
+|
+|_____
+"""]
+
+
+d_list = [
+'abruptly', 
+'absurd', 
+'abyss', 
+'affix', 
+'askew', 
+'avenue', 
+'awkward', 
+'axiom', 
+'azure', 
+'bagpipes', 
+'bandwagon', 
+'banjo', 
+'bayou', 
+'beekeeper', 
+'bikini', 
+'blitz', 
+'blizzard', 
+'boggle', 
+'bookworm', 
+'boxcar', 
+'boxful', 
+'buckaroo', 
+'buffalo', 
+'buffoon', 
+'buxom', 
+'buzzard', 
+'buzzing', 
+'buzzwords', 
+'caliph', 
+'cobweb', 
+'cockiness', 
+'croquet', 
+'crypt', 
+'curacao', 
+'cycle', 
+'daiquiri', 
+'dirndl', 
+'disavow', 
+'dizzying', 
+'duplex', 
+'dwarves', 
+'embezzle', 
+'equip', 
+'espionage', 
+'euouae', 
+'exodus', 
+'faking', 
+'fishhook', 
+'fixable', 
+'fjord', 
+'flapjack', 
+'flopping', 
+'fluffiness', 
+'flyby', 
+'foxglove', 
+'frazzled', 
+'frizzled', 
+'fuchsia', 
+'funny', 
+'gabby', 
+'galaxy', 
+'galvanize', 
+'gazebo', 
+'giaour', 
+'gizmo', 
+'glowworm', 
+'glyph', 
+'gnarly', 
+'gnostic', 
+'gossip', 
+'grogginess', 
+'haiku', 
+'haphazard', 
+'hyphen', 
+'iatrogenic', 
+'icebox', 
+'injury', 
+'ivory', 
+'ivy', 
+'jackpot', 
+'jaundice', 
+'jawbreaker', 
+'jaywalk', 
+'jazziest', 
+'jazzy', 
+'jelly', 
+'jigsaw', 
+'jinx', 
+'jiujitsu', 
+'jockey', 
+'jogging', 
+'joking', 
+'jovial', 
+'joyful', 
+'juicy', 
+'jukebox', 
+'jumbo', 
+'kayak', 
+'kazoo', 
+'keyhole', 
+'khaki', 
+'kilobyte', 
+'kiosk', 
+'kitsch', 
+'kiwifruit', 
+'klutz', 
+'knapsack', 
+'larynx', 
+'lengths', 
+'lucky', 
+'luxury', 
+'lymph', 
+'marquis', 
+'matrix', 
+'megahertz', 
+'microwave', 
+'mnemonic', 
+'mystify', 
+'naphtha', 
+'nightclub', 
+'nowadays', 
+'numbskull', 
+'nymph', 
+'onyx', 
+'ovary', 
+'oxidize', 
+'oxygen', 
+'pajama', 
+'peekaboo', 
+'phlegm', 
+'pixel', 
+'pizazz', 
+'pneumonia', 
+'polka', 
+'pshaw', 
+'psyche', 
+'puppy', 
+'puzzling', 
+'quartz', 
+'queue', 
+'quips', 
+'quixotic', 
+'quiz', 
+'quizzes', 
+'quorum', 
+'razzmatazz', 
+'rhubarb', 
+'rhythm', 
+'rickshaw', 
+'schnapps', 
+'scratch', 
+'shiv', 
+'snazzy', 
+'sphinx', 
+'spritz', 
+'squawk', 
+'staff', 
+'strength', 
+'strengths', 
+'stretch', 
+'stronghold', 
+'stymied', 
+'subway', 
+'swivel', 
+'syndrome', 
+'thriftless', 
+'thumbscrew', 
+'topaz', 
+'transcript', 
+'transgress', 
+'transplant', 
+'triphthong', 
+'twelfth', 
+'twelfths', 
+'unknown', 
+'unworthy', 
+'unzip', 
+'uptown', 
+'vaporize', 
+'vixen', 
+'vodka', 
+'voodoo', 
+'vortex', 
+'voyeurism', 
+'walkway', 
+'waltz', 
+'wave', 
+'wavy', 
+'waxy', 
+'wellspring', 
+'wheezy', 
+'whiskey', 
+'whizzing', 
+'whomever', 
+'wimpy', 
+'witchcraft', 
+'wizard', 
+'woozy', 
+'wristwatch', 
+'wyvern', 
+'xylophone', 
+'yachtsman', 
+'yippee', 
+'yoked', 
+'youthful', 
+'yummy', 
+'zephyr', 
+'zigzag', 
+'zigzagging', 
+'zilch', 
+'zipper', 
+'zodiac', 
+'zombie', 
+]
+
+choice = random.randint(0, len(d_list) - 1)
+
+word = d_list[choice]
+
+print(hangman[0])
+
+display =  ["_"] * len(word)
+
+# for i in word: 
+#     ''.join([display, '_'])
+
+mistake = 0
+
+print(''.join(display))
+
+while mistake < 6:
+    guess = input("Guess a letter: ")
+
+    guess = guess.lower()
+    found = False
+
+    for index, char in enumerate(word): 
+        if char == guess: 
+            display[index] = char
+            found = True
+        
+    if not found: 
+        mistake += 1
+    else: 
+        found = False
+
+    print(hangman[mistake])
+    print(f'**********************************{6 - mistake} / 6 Lives Left **********************************')
+    
+    print(''.join(display))
+
+    if '_' not in display: 
+        print("**********************************You won!**********************************")
+        sys.exit()
+
+    
+print(hangman[mistake + 1])
+print("**********************************You lose!**********************************")
